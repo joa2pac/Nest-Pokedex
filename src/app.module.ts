b260d02bdfdc6449625b2475da912dffda8b08b6
joaquin <joaquinahuel93@gmail.com>
2024-06-04 16:30:47 -0300

    conected nest with mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 
@@ -11,6 +12,9 @@ import { PokemonModule } from './pokemon/pokemon.module';
     ServeStaticModule.forRoot({ 
         rootPath: join(__dirname, '..', 'public')
     }),
+
+MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+
     PokemonModule 
   ],
   controllers: [AppController],
