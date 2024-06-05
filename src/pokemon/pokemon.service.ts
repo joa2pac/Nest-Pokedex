50cfc9225493e3ad06544f614f87ab87ed69787a
joaquin <joaquinahuel93@gmail.com>
2024-06-05 18:49:35 -0300

    add find by name, mongo id o no

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,17 +1,19 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
-import { Model } from 'mongoose';
+import { Model, isValidObjectId } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 
 @Injectable()
 export class PokemonService {
 
-constructor(
-  @InjectModel(Pokemon.name)
-  private readonly pokemonModel: Model<Pokemon>
-) {}
+  constructor(
+    
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel: Model<Pokemon>,
+
+  ) {}
 
 
   async create(createPokemonDto: CreatePokemonDto) {
@@ -30,7 +32,6 @@ constructor(
     }
 
 
-
     
   }
 
@@ -38,8 +39,27 @@ constructor(
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(term: string) {
+
+  let pokemon: Pokemon;
+
+  if(!isNaN(+term)) {
+
+  pokemon = await this.pokemonModel.findOne({no: term});
+}
+
+if( !pokemon && isValidObjectId(term)) {
+  pokemon = await this.pokemonModel.findById(term);
+}
+
+if ( !pokemon ) {
+  pokemon = await this.pokemonModel.findOne({ name: term.toLowerCase().trim() })
+}
+
+if (!pokemon) 
+  throw new NotFoundException(`Pokemon with id, name or no "${term}" not found`);
+
+ return pokemon;
   }
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
