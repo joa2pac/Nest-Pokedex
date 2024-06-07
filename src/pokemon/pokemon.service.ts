25543adf583d418e3a19c65795b7cb7516e0d1b6
joaquin <joaquinahuel93@gmail.com>
2024-06-06 22:32:43 -0300

    validated and deleted only consulting

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -82,8 +82,13 @@ try {
   async remove(id: string) {
     // const pokemon = await this.findOne(id);
     // await pokemon.deleteOne();
-    const result = this.pokemonModel.findByIdAndDelete(id)
-    return result;
+    // const result =  await this.pokemonModel.findByIdAndDelete(id)
+
+    const {deletedCount} = await this.pokemonModel.deleteOne({_id: id});
+    if( deletedCount === 0)
+      throw new BadRequestException(`Pokemon with id "${id}" not found`);
+
+    return;
   }
 
 private handleExceptions(error: any) {
