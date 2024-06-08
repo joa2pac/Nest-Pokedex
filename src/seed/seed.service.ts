d9d91b898fc2bbcc94dec520001e5a7f9f42535d
joaquin <joaquinahuel93@gmail.com>
2024-06-07 23:33:27 -0300

    uptdate

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
 
 
 @Injectable()
 export class SeedService {
+
+  private readonly axios: AxiosInstance = axios;
   
-executeSeed() {
-  return 'Seed executed'
+async executeSeed() {
+
+  const {data} = await this.axios.get(`https://pokeapi.co/api/v2/pokemon?limit=650`);
+
+
+  return data.results;
 }
 
 
